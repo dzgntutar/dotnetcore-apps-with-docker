616f6e1e80c27452bb08f55a4e296ffadb9cba1b
dutarrr <duzguntutar@gmail.com>
2021-08-04 17:33:20 +0300

    console app with docker

---
+++ 1-ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var counter = 0;
+            while (counter < 5)
+            {
+                Console.WriteLine($"Counter: {++counter}");
+                await Task.Delay(500);
+            }
+        }
+    }
+}
