57f694fd06eabe46816baabc742f517f108e0b17
dutarrr <duzguntutar@gmail.com>
2021-08-08 17:29:37 +0300

    docker compose integraiton done

---
+++ 3-ApiApp-With-Sql-Server/ABC.Entity/Concrete/Hello.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ABC.Service
+{
+    public class Hello
+    {
+        public string Message { get; set; }
+        public string Message2 { get; set; }
+    }
+}
---
+++ 3-ApiApp-With-Sql-Server/ABC.Service/Controllers/HelloController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ABC.Service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public Hello Get()
+        {
+            var hello = new Hello();
+            hello.Message = "Hi";
+            hello.Message2 = "How are you";
+            return  hello;
+        }
+    }
+}
