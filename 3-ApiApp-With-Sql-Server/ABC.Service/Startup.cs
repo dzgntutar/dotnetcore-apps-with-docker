06da53457944ada7b04b7fc2a5ce9622cfab1725
dutarrr <duzguntutar@gmail.com>
2021-08-08 12:43:25 +0300

    enttiy referance added to service project

--- 3-ApiApp-With-Sql-Server/ABC.Service/Startup.cs
+++ 3-ApiApp-With-Sql-Server/ABC.Service/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 
 namespace ABC.Service
 {
@@ -28,10 +27,6 @@ namespace ABC.Service
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ABC.Service", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +35,6 @@ namespace ABC.Service
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ABC.Service v1"));
             }
 
             app.UseHttpsRedirection();
